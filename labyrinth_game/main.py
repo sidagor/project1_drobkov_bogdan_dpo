df3cf6a7dc91a7673ab42788f6c82bbe8e81384a
Drobkov Bogdan <0brotishka0@gmail.com>
2025-11-11 19:44:15 +0300

    feat: complete game architecture setup

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+from labyrinth_game import constants
+from labyrinth_game import player_actions
+from labyrinth_game import utils
+
+game_state = {
+    'player_inventory': [],  # Инвентарь игрока
+    'current_room': 'entrance',  # Текущая комната
+    'game_over': False,  # Флаг окончания игры
+    'steps_taken': 0  # Количество шагов
+}
+
 def main():
     print("Первая попытка запустить проект!")
 
